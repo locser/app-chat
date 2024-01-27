97c2cf4960133f81de410fcfc522fdb28b8d0a93
locph <locph@overate-vntech.com>
2024-01-27 14:45:23 +0700

    config .env

--- src/config-mongo/config-mongo.service.ts
+++ src/config-mongo/config-mongo.service.ts
@@ -4,7 +4,7 @@ import { MongooseModuleOptions } from '@nestjs/mongoose';
 @Injectable()
 export class ConfigMongoService {
   async createMongooseOptions(): Promise<MongooseModuleOptions> {
-    const uriString = `${'mongodb://172.16.0.127:27017/'}${'app-chat'}${'?retryWrites=true&w=majority'}`;
+    const uriString = `${process.env.MONGODB_HOST}${process.env.MONGODB_DBNAME}${process.env.MONGODB_OPTIONS}`;
     return {
       // uri: `${process.env.CONFIG_MONGOOSE_HOST}${process.env.CONFIG_MONGOOSE_DB_NAME}${process.env.CONFIG_MONGOOSE_OPTION}`,
       uri: uriString,
